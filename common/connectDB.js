d70db7ecaaa02eb99d36b06d6524a397a62e1b84
lambiengcode <lambiengcode@gmail.com>
2021-02-27 18:14:21 +0700

    setup .env success

--- common/connectDB.js
+++ common/connectDB.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 
 export const connectDatabase = () => {
   mongoose.Promise = require('bluebird')  
-  // mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URLDEV || process.env.DB_URL}/${process.env.DB_NAME}`, {
-  mongoose.connect(`mongodb://${process.env.DB_URL}/${process.env.DB_NAME}`, {
+  mongoose.connect(`${process.env.DB_URL}`, {
+  //mongoose.connect(`mongodb://${process.env.DB_URL}/${process.env.DB_NAME}`, {
     useUnifiedTopology: true,
     useFindAndModify: false,
     useNewUrlParser: true,
